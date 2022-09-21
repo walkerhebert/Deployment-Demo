2ff0f456bc9fcb230811ce6a53bc7286d988ff5b
Walker Hebert <walkerhebert@yahoo.com>
2022-09-21 13:23:35 -0500

    Deployment-demo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 require('dotenv').config()
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 5000
 
 const app = express()
 
