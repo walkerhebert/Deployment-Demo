30493c3ba214809028b6120ac37041d11f708d4a
Walker Hebert <walkerhebert@yahoo.com>
2022-09-21 13:27:13 -0500

    Deployment-demo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(path.join__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.listen(port, () => {
