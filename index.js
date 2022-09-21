0daf2776a9e1f7b427a48cf22fca37c38de51a89
Walker Hebert <walkerhebert@yahoo.com>
2022-09-21 12:55:43 -0500

    Final

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+
+const port = process.env.PORT || 6000
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.join__dirname, './index.html'))
+})
+
+app.listen(port, () => {
+    console.log('Docked at port ' + port)
+})
